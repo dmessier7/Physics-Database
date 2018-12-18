8b135fb61b5708a21686cf9ffabb82e5934a19b7
dmessier7 <daniel.messier.11.12@gmail.com>
2018-12-17 20:26:39 -0800

    Update MultiTableLabDatabase.py
    cleaning up

--- MultiTableLabDatabase.py
+++ MultiTableLabDatabase.py
@@ -1,73 +1,3 @@
-"""
-
-Three Normal Forms:
-    1. No Repeating Groups
-    2. Data Dependancy on Key
-    3. No Redundant Data
-
-Features:
-    Main Menu
-        Class List :Phys 250, Phys 270, etc. (each opens up their Class tab)
-        Search  for Item (gives Item tab)
-        Lab List
-        Item List
-        Add Item (adds Item to the Item Database)
-            -UI should show similar Items while you type in the name of the new one,
-                because it only stops against exact name dublicates
-        Search for Lab (gives Lab Tab)
-        Add Lab (add Lab to Lab Database)
-        Add Class (adds Class to Class Database)
-
-    Class Tab (eg Phys 250)
-        Class Name
-
-        Lab List: Free Fall, Projectile Motion, etc (each opens up their Lab tab)
-    Lab Tab (eg Projectile Motion, Ohm's Law)
-        Lab Name
-        Equipment Table, with each row:
-            Number of this item thats required for a table
-            Item (Opens up to Item tab)
-            Location of Item
-            Quantity of Item
-        Lab Pictures
-        Lab Description
-        List of Classes this Lab is in
-    Item tab
-        Item Name
-        Item Quantity
-        Item Location (Edits location in all Lab Tabs that contain that Item)
-        Item Description
-        Pictures
-
-
-
-Coding
-Location is a Class X
-Function Edit Quantity X
-Function Edit Location X
-Function Edit Description X
-Function Edit Name X
-Item is a Class
-    ItemID
-    string Name
-    string Description
-
-InventoryTable
-    ItemID
-    str Name
-    str Description
-LocationTable
-    LocationID
-    L0, L1, L2, L3, L4, L5
-QuantityTable
-    ItemID
-    LocationID
-    Quantity
-
-
-
-"""
-
 import sqlite3 as s
 import sys
 import random
@@ -76,7 +6,7 @@ from PyQt5 import QtGui
 from PyQt5 import QtWidgets
 
 
-# Item classes and functions
+# Item classes and Functions
 
 # Location is a Array of Strings for the location of the item
 class Location:
